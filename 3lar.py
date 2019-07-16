d9c33cbf40d1b4cbca3d185c15629fb72d42a0be
shebilantony <42166928+shebilantony@users.noreply.github.com>
2019-07-16 14:00:34 +0530

    Update and rename 3lar to 3lar.py

--- 3lar.py
+++ 3lar.py
@@ -2,9 +2,9 @@ num1=float(input("Enter first no:"))
 num2=float(input("Enter second no:"))
 num3=float(input("Enter third no:"))
 if(num1>num2) and (num1>num3):
-largest=num1
+  largest=num1
 elif(num2>num1) and (num2>num3):
-largest=num2
+  largest=num2
 else:
-largest=num3
+  largest=num3
 print("the largest no is",largest)
